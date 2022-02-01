cb2ac9dfa2bc5470c0340dfeaed625ab7b16c6b3
KimYeSeung <alclsto1@gmail.com>
2022-02-02 04:21:38 +0900

    login.html 작성 urls.py에 login 패스 추가
    board\views.py 에 login 함수 추가

--- board/models.py
+++ board/models.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-# Create your models here.
-class Board(models.Model):
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    contents = models.TextField()
-    create_date = models.DateTimeField(auto_now_add=True)
+# from django.contrib.auth.models import User
+# from django.db import models
+#
+# # Create your models here.
+# class Board(models.Model):
+#     writer = models.ForeignKey(User, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=200)
+#     contents = models.TextField()
+#     create_date = models.DateTimeField(auto_now_add=True)
